ac225766c4cac0d101e7ad3b4e22d3ba21d2271d
andreaszapf <andreaszapf@users.noreply.github.com>
2023-01-01 22:07:38 +0100

    Make the file enumeration callback based
    To test how Hörbär's find_media enumeration, which is also callback based, could
    be integrated into a flutter app.
    
    To keep the iterative interface, a background thread runs the enumeration, and a
    blocking fixed-capacity SPSC queue is used to transfer the entries to the
    calling thread.
    
    For simplicity, this implementation spawns a new thread for each enumeration --
    in production something like a thread pool should be used.

--- shared/find-audiofiles/src/find_audiofiles.cpp
+++ shared/find-audiofiles/src/find_audiofiles.cpp
@@ -1,17 +1,23 @@
 #include "find_audiofiles/find_audiofiles.h"
 
+#include <readerwritercircularbuffer.h>
+
 #include <algorithm>
 #include <array>
-#include <cassert>
+#include <atomic>
 #include <filesystem>
 #include <memory>
 #include <optional>
+#include <thread>
 #include <variant>
 
 namespace fs = std::filesystem;
 
 namespace
 {
+
+constexpr size_t queue_capacity = 1;
+
 using directory_iterator = fs::recursive_directory_iterator;
 
 bool is_audio_file_path(const fs::path& path) noexcept
@@ -78,18 +84,16 @@ public:
     return entry_.has_value() ? &(*entry_) : nullptr;
   }
 
-  void update(const audiofile_iterator& iterator) noexcept
+  void update(const fs::path& path) noexcept
   {
-    if (iterator.at_end())
-    {
-      entry_ = std::nullopt;
-      entry_path_storage_.clear();
-    }
-    else
-    {
-      entry_path_storage_ = iterator.current().path().u8string();
-      entry_ = faf_filesystem_entry{entry_path_storage_.c_str()};
-    }
+    entry_path_storage_ = path.u8string();
+    entry_ = faf_filesystem_entry{entry_path_storage_.c_str()};
+  }
+
+  void update(const std::error_code& error_code) noexcept
+  {
+    entry_ = std::nullopt;
+    entry_path_storage_.clear();
   }
 
 private:
@@ -97,32 +101,105 @@ private:
   std::string entry_path_storage_;
 };
 
+template <typename Callback>
+int enumerate_audiofiles(const fs::path& directory, Callback&& callback) noexcept
+{
+  std::error_code error;
+  auto iterator = audiofile_iterator(directory, error);
+
+  if (error)
+  {
+    return error.value();
+  }
+
+  int callback_return_value = 0;
+  for (; !iterator.at_end() && callback_return_value == 0; iterator.move_next())
+  {
+    callback_return_value = callback(iterator.current().path());
+  }
+
+  return callback_return_value;
+}
+
+using message = std::variant<std::error_code, fs::path>;
+
 } // namespace
 
 class faf_context
 {
 public:
-  faf_context(fs::path path, std::error_code& error) noexcept
-    : iterator_{path, error}
+  faf_context(const fs::path& directory, std::error_code& error) noexcept
   {
-    entry_wrapper_.update(iterator_);
+    enumerator_thread_ = std::thread(
+      [this, directory = directory] { enumerator_thread_function(directory); });
+
+    error = process_next_message();
+    if (error)
+    {
+      try
+      {
+        enumerator_thread_.join();
+      }
+      catch (const std::exception&)
+      {
+      }
+    }
   }
+
+  ~faf_context()
+  {
+    try
+    {
+      is_active_.store(false);
+      // Make sure that enumerator_thread_ isn't blocking on a full queue.
+      message msg;
+      queue_.try_dequeue(msg);
+      enumerator_thread_.join();
+    }
+    catch (const std::exception&)
+    {
+    }
+  }
+
+  faf_context(const faf_context&) = delete;
+  faf_context& operator=(const faf_context&) = delete;
 
   faf_filesystem_entry* entry() noexcept { return entry_wrapper_.entry(); }
 
-  std::error_code move_next() noexcept
+  std::error_code process_next_message()
   {
-    const auto error = iterator_.move_next();
-    entry_wrapper_.update(iterator_);
-    return error;
+    message msg;
+    queue_.wait_dequeue(msg);
+    std::visit([&](const auto& value) { entry_wrapper_.update(value); }, msg);
+    return std::holds_alternative<std::error_code>(msg) ? std::get<std::error_code>(msg)
+                                                        : std::error_code{};
   }
 
 private:
-  audiofile_iterator iterator_;
+  void enumerator_thread_function(const fs::path& directory) noexcept
+  {
+    const auto result = enumerate_audiofiles(
+      directory, [this](const fs::path& path) { return post(path) ? 0 : -1; });
+    post(std::error_code{result, std::system_category()});
+  }
+
+  bool post(message&& msg) noexcept
+  {
+    if (is_active_.load())
+    {
+      queue_.wait_enqueue(std::move(msg));
+      return true;
+    }
+    return false;
+  }
+
+  moodycamel::BlockingReaderWriterCircularBuffer<message> queue_{queue_capacity};
+  std::thread enumerator_thread_;
+  std::atomic<bool> is_active_{true};
   filesystem_entry_wrapper entry_wrapper_;
 };
 
-int faf_first(const char* path, faf_context** context, faf_filesystem_entry** entry)
+int faf_first(const char* directory, faf_context** context, faf_filesystem_entry** entry)
 {
   if (context)
   {
@@ -134,13 +211,13 @@ int faf_first(const char* path, faf_context** context, faf_filesystem_entry** en
     *entry = nullptr;
   }
 
-  if (path == nullptr || context == nullptr || entry == nullptr)
+  if (directory == nullptr || context == nullptr || entry == nullptr)
   {
     return std::make_error_code(std::errc::invalid_argument).value();
   }
 
   std::error_code error;
-  auto new_context = std::make_unique<faf_context>(fs::u8path(path), error);
+  auto new_context = std::make_unique<faf_context>(fs::u8path(directory), error);
   if (!error)
   {
     *entry = new_context->entry();
@@ -162,7 +239,7 @@ int faf_next(faf_context* context, faf_filesystem_entry** entry)
     return std::make_error_code(std::errc::invalid_argument).value();
   }
 
-  const auto error = context->move_next();
+  const auto error = context->process_next_message();
   *entry = context->entry();
   return error.value();
 }
